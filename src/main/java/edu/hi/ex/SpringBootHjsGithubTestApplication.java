4f5f228a455abf03e7a5c73d07a41df3402ddd44
TEACHER\PC-00 <nqwrt@naver.com>
2022-07-11 10:16:26 +0900

    두번째 소스코드 테스트 머지

--- src/main/java/edu/hi/ex/SpringBootHjsGithubTestApplication.java
+++ src/main/java/edu/hi/ex/SpringBootHjsGithubTestApplication.java
@@ -3,6 +3,8 @@ package edu.hi.ex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//github 테스트
 @SpringBootApplication
 public class SpringBootHjsGithubTestApplication {
 
