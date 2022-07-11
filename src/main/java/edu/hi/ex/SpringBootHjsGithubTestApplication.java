5a97ed99a588720c6e9c0648dd9ff5f360dbeda6
TEACHER\PC-00 <nqwrt@naver.com>
2022-07-11 10:06:21 +0900

    first commit

---
+++ src/main/java/edu/hi/ex/SpringBootHjsGithubTestApplication.java
@@ -0,0 +1,13 @@
+package edu.hi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHjsGithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHjsGithubTestApplication.class, args);
+	}
+
+}
